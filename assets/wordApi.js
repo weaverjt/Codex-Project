b3b9ad84cbf18671e716bfded2e9822a4faabcda
Weaverjt <weaver.jacob.t@gmail.com>
2019-01-14 18:25:06 -0600

    base

---
+++ assets/wordApi.js
@@ -0,0 +1,18 @@
+// I need to create an object to contain character things...
+
+
+/*
+Create a thing to store the characters, factions, etc?...or at least save & store the data in the database...
+
+As well as a thing to edit/delete the user object data
+*/
+
+//NEW CHARACTER
+
+//NEW FACTION
+
+//NEW PLACE
+
+//NEW EVENT
+
+//NOTES (maybe also yes?)
